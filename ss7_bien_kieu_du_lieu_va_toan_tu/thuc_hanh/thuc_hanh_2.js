2509b522f875e7e357ca602e0a8bbd43e7d7190f
Meimei38 <uyennguyencat@gmail.com>
2022-07-06 15:10:49 +0700

    nop bai ngay 6-7

---
+++ ss7_bien_kieu_du_lieu_va_toan_tu/thuc_hanh/thuc_hanh_2.js
@@ -0,0 +1,6 @@
+inputWidth= prompt('Enter the width: ');
+inputHeight= prompt('Enter the height: ');
+let Width= parseInt(inputWidth);
+let Height=parseInt(inputHeight);
+let area= Width*Height;
+document.write('The area is: '+area);
